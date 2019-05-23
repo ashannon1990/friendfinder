9936a67919efd39f9a484b096c95b41e48f6c1ae
John Calvin <ashannon1990@gmail.com>
2019-05-23 19:25:42 -0400

    having trouble with comparisons

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,13 +2,33 @@ var friendsData = require("../data/friends")
 
 
 
-module.exports = function(app) {
-app.get("/api/friends", function(req, res) {
+module.exports = function (app) {
+  app.get("/api/friends", function (req, res) {
     res.json(friendsData)
-})
-app.post("/api/friends", function(req, res) {
+  })
+  app.post("/api/friends", function (req, res) {
     friendsData.push(req.body);
     res.json(true)
-})
+
+    var user = req.body;
+
+    //  convert user input to integers
+    for (var i = 0; i < user.scores.length; i++) {
+      user.scores[i] = parseInt(user.scores[i])
+    }
+    //  compare user input to people in the api, and find the total difference between answers
+    var totalDifference = []
+    var currentDifference
+    for (var i = 0; i < friendsData[i].length; i++) {
+      currentDifference = 0
+      for (var j = 0; friendsData[i].scores.length; j++) {
+        difference += (Math.abs(req.body.scores[j] - friendsData[i].scores[j]));
+      }
+      totalDifference.push(currentDifference);
+
+    }
+    // send back results to html page so they can be added to modal
+    friendsData.push(req.body);
+  });
 }
 
