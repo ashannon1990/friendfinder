07a5ce4658322ac37f30c695fbc09722d1a6480f
John Calvin <ashannon1990@gmail.com>
2019-05-24 23:16:54 -0400

    Finished functionality and created readme

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friendsData = [{
-  name: "Ahmed",
-  photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  name: "Shane",
+  photo: "https://pbs.twimg.com/profile_images/712013886765944832/u8Zjj19f_400x400.jpg",
   scores:
     [5,
       1,
@@ -15,7 +15,7 @@ var friendsData = [{
 },
 {
   name: "Stacy",
-  photo: "placeholder",
+  photo: "https://s3.amazonaws.com/media.muckrack.com/profile/images/27627/nyckennedy.png.256x256_q100_crop-smart.png",
   scores:
     [3,
       2,
@@ -30,7 +30,7 @@ var friendsData = [{
 },
 {
   name: "Desmond",
-  photo: "placeholder",
+  photo: "https://pm1.narvii.com/6820/a7bcf8ee2c9ef8540b778961967793f91dc976d6v2_128.jpg",
   scores:
     [4,
       4,
@@ -42,6 +42,36 @@ var friendsData = [{
       2,
       3,
       1]
+},
+{
+  name: "Jack",
+  photo: "https://vignette.wikia.nocookie.net/biches-of-jacksfilms/images/d/d1/34fp48PD_400x400.png/revision/latest?cb=20150808123313",
+  scores:
+    [2,
+      4,
+      1,
+      1,
+      4,
+      2,
+      3,
+      4,
+      2,
+      3]
+},
+{
+  name: "Maria",
+  photo: "https://d2wldr9tsuuj1b.cloudfront.net/21843/pictures-thumb/2019/1/Santa%20Maria_Amanda_416.jpg",
+  scores:
+    [4,
+      2,
+      3,
+      3,
+      3,
+      5,
+      4,
+      4,
+      3,
+      5]
 }]
 
 module.exports = friendsData
