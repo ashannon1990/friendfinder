3d1d3a8dc51a115fe633bbf5fb72995edcff11c9
John Calvin <ashannon1990@gmail.com>
2019-05-25 08:48:29 -0400

    added comment

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -35,12 +35,12 @@ module.exports = function (app) {
       if (currentDifference === leastDifference) {
         console.log("Friend found: " + friendsData[i].photo);
         var friendFound = friendsData[i].name;
-        var photoFound = friendsData[i].photo;
+        var friendPhoto = friendsData[i].photo;
         res.json(friendsData[i]);
 
       };
     }
-
+// pushing to api on local, but not on deployed
     friendsData.push(req.body);
 
 
